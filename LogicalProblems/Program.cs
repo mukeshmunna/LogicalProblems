ce40389d3763c9f44afdf357c6c3ff980984221d
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-07-13 16:33:19 +0530

    [Mukesh] add:created a fibonacci series

---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,23 @@
+﻿using LogicalProblem;
+using System;
+namespace LogicalProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the input :");
+            Console.WriteLine("1.Fibonacci series");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    Fibonacci.Fib();
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
